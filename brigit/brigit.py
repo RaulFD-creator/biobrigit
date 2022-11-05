ccc830de211d9c29ef7ea0e76433404869b39943
RaulFD-creator <raulfdz9@gmail.com>
2022-11-05 10:10:15 +0100

    Documentation

--- brigit/brigit.py
+++ brigit/brigit.py
@@ -1,5 +1,5 @@
 """
-Main Brigit module.
+Main BrigitMetalPredictor module.
 
 Contains 1 class:
     - Brigit
@@ -8,6 +8,7 @@ Copyright by Raúl Fernández Díaz
 """
 import time
 import multiprocessing
+import warnings
 import torch
 import numpy as np
 from sklearn.cluster import Birch
@@ -45,14 +46,261 @@ class Brigit():
         model: str,
         **kwargs
     ):
+        """
+        Initialize a new instance of the class.
+        Set CUDA up if any GPU device is available.
+        """
         # TODO: Verify that cuda device is available before setting it
         # up.
         if device == 'cuda':
-            self.device = f'{device}:{device_id}'
-            set_up_cuda(device_id)
+            if torch.cuda.is_available():
+                self.device = f'{device}:{device_id}'
+                set_up_cuda(device_id)
+            else:
+                warnings.warn('Warning: No CUDA compatible GPU was available,\
+                    defaulting to CPU.')
+                self.device = 'cpu'
         else:
             self.device = device
+            if torch.cuda.is_available():
+                warnings.warn('Using CPU for caluclations, although there is\
+                    a CUDA compatible GPU available. Using the GPU would\
+                    accelerate significantly the calculations.')
         self.model = load_model(model, device)
+
+    def predict(
+        self,
+        target: str,
+        metal: str,
+        max_coordinators: int = 4,
+        outputfile: str = None,
+        cnn_threshold: float = 0.5,
+        combined_threshold: float = 0.5,
+        verbose: int = 1,
+        clustering_radius: float = 5.0,
+        cnn_weight: float = 0.5,
+        **kwargs
+    ) -> np.array:
+        """
+        Main method of the class. It takes a protein target and it
+        predicts the suitability of its regions for coordinating metals.
+
+        First, the protein is voxelized using the Moleculekit library. 6
+        channels are computed: hidrophobicity, H-bond acceptance, H-bond
+        reception, positive ionizability, negative ionizability, excluded
+        volume. The voxelized representation is parsed with the CNN and each
+        point is evaluated. Then, all points with scores superior to the
+        threshold are further analysed. In this case, their relative position
+        to key atoms in the protein backbone will provide a statistical score
+        that serves as indicator as to how well coordinated a hypothetical
+        metal ion will be.
+
+        The resulting points will be clustered attending to their spatial
+        coordinates and the centers of the clusters will be assigned as
+        a weighted mean of the coordinates of all points within the cluster.
+        The weights will be the square of the scores. So, that the center is
+        closer to highly coordinable regions.
+
+        Finally, two output files will be generated:
+            1. A '_brigit.pdb' file which contains all probes with a score
+                superior to `combined_threshold`.
+            2. A '.clusters' file which contains all residues within
+                `clustering_radius` of every cluster center. Thus, summarising
+                the most likely coordinating residues in the protein. Each
+                cluster will have assigned a global score which will be a sum
+                of the scores of all the probes that comprise it.
+
+        Args:
+            target (str): Path to the PDB file or PDB code of the target
+                protein.
+            metal (str): IUPAC symbol of the metal to be evaluated. There is a
+                mode, 'General', that uses statistics built from all metals
+                simultaneously.
+            max_coordinators (int, optional): Maximum number of coordinators
+                expected. Value affects the sensibility of the coordination
+                analysis. Defaults to 4.
+            outputfile (str, optional): Path where the output files are to
+                be stored. It is recommended that it does not contain tags,
+                e.g., '.pdb', as the program itself will asign them.
+                Defaults to None.
+            cnn_threshold (float, optional): Score threshold for the output of
+                the CNN model to be further analysed. Defaults to 0.5.
+            combined_threshold (float, optional): Final threshold to consider a
+                region. Defaults to 0.5.
+            verbose (int, optional): Ammount of information to be displayed
+                during computation. Defaults to 1.
+            clustering_radius (float, optional): Radius of the clusters used to
+                organise the results. Affects mainly the final summary.
+                Defaults to 5.0.
+            cnn_weight (float, optional): Weight of the CNN for the final
+                score. Defaults to 0.5.
+
+        Raises:
+            TypeError: In case the target is neither a PDB code nor a Path to a
+                local PDB file.
+
+        Returns:
+            np.array: Set of coordinates and their score.
+        """
+        start = time.time()
+        verbose = bool(verbose)
+        if not isinstance(target, str):
+            message = 'Target has to be:\n  a) string with PDB ID or\n'
+            message += '  b) path to local PDB file.'
+            raise TypeError(message)
+
+        if combined_threshold > cnn_threshold:
+            cnn_threshold = combined_threshold
+
+        if verbose:
+            print(f'Voxelizing target: {target}', end='\n\n')
+
+        vox, p_centers, nvoxels = self.voxelize(target, **kwargs)
+
+        if verbose:
+            print(f'\nCNN evaluation of target: {target}', end='\n\n')
+
+        scores = self.evaluate(vox, p_centers, **kwargs)
+
+        if verbose:
+            print(f'\nCoordination analysis of target: {target}', end='\n\n')
+
+        scores, molecule = self.coordination_analysis(
+            target, max_coordinators, metal, scores, cnn_threshold,
+            verbose, kwargs['residue_score'], kwargs['backbone_score'],
+            cnn_weight, kwargs
+        )
+        best_scores = np.argwhere(scores[:, 3] > combined_threshold)
+        new_scores = np.zeros((len(best_scores), 4))
+        for idx, idx_score in enumerate(best_scores):
+            new_scores[idx, :] = scores[idx_score, :]
+        centers = self.clusterize(
+            new_scores, molecule, clustering_radius
+        )
+
+        if outputfile is None:
+            if target.endswith('.pdb'):
+                outputfile = target.split('.')[0]
+            else:
+                outputfile = target
+        self.create_PDB(
+            target, outputfile, new_scores, combined_threshold, centers,
+            molecule
+        )
+        self.check_clusters(centers, molecule, outputfile, kwargs['args'])
+        end = time.time()
+        if verbose:
+            print(f'Computation took {round(end-start, 2)} s.', end='\n\n')
+        return scores
+
+    def coordination_analysis(
+        self,
+        target: protein,
+        max_coordinators: int,
+        metal: str,
+        scores: np.array,
+        threshold: float,
+        verbose: bool,
+        residue_score: str,
+        backbone_score: str,
+        cnn_weight: float,
+        residues: list,
+        threads: int,
+        **kwargs
+    ) -> tuple:
+        """
+        It uses a series of statistical values regarding backbone
+        preorganization of metal-binding amino acids, to evaluate
+        how well coordinated each probe would be.
+
+        There are 2 main functions that can be used for this scoring:
+            1. 'discrete': which uses the median values of the statistics
+                and evaluates whether a certain geometric requirements are
+                fulfilled or not.
+            2. 'gaussian': which instead of using a boolean argument, scores
+                more positively those regions as they are closer to the actual
+                central values in a continuos fashion.
+
+        More details on these functions can be found in the Docstrings for
+        the module `utils/scoring`.
+
+        Args:
+            target (protein): Path to the PDB file or PDB code of the target
+                protein.
+            max_coordinators (int): Maximum number of coordinators
+                expected. Value affects the sensibility of the coordination
+                analysis.
+            metal (str): IUPAC symbol of the metal to be evaluated. There is a
+                mode, 'General', that uses statistics built from all metals
+                simultaneously.
+            scores (np.array): Array with dimensions (len(probes), 4) where the
+                first 3 dimensions describe the cartesian coordinates occupied
+                by the probe, and the fourth is its score.
+            threshold (float): (CNN) score value that determines whether the
+                coordination of a certain probe will be analysed or not.
+            verbose (bool): Ammount of information to be displayed
+                during computation.
+            residue_score (str): Scoring function used to evaluate the
+                coordination through side chains.
+            backbone_score (str): Scoring function used to evaluate the
+                coordination through backbone atoms.
+            cnn_weight (float): Proportion of the final score that will be
+                informed by the CNN. Its complementary (1 - cnn_weight)
+                corresponds to the proportion of the final score informed
+                by the coordination analysis.
+            residues (list): List with the most likely residue coordinators for
+                any specific metal.
+            threads (int): Number of threads to be used during the
+                voxelization.
+
+        Raises:
+            IndexError: Occurs in the event that the residue coordination
+                scoring function selected is not implemented.
+            IndexError: Occurs in the event that the backbone coordination
+                scoring function selected is not implemented.
+
+        Returns:
+            tuple: (np.array, protein) the np.array corresponds to
+                the updated scores array with the cartesian coordiantes
+                of the probes and their scores.
+        """
+        zeros = np.zeros(np.shape(scores[:, 3]))
+        molecule = protein(target, True)
+        coordinators, stats, gaussian_stats = find_most_likely_coordinators(
+            metal, kwargs['residues']
+        )
+        if residue_score == 'discrete':
+            residue_score = discrete_score
+        elif residue_score == 'gaussian':
+            residue_score = gaussian_score
+        else:
+            raise IndexError(
+                f'Residue coordination scoring function: {residue_score} not\
+                    currently implemented.'
+            )
+
+        if backbone_score == 'discrete':
+            backbone_score = discrete_score
+        else:
+            raise IndexError(
+                f'Backbone coordination scoring function: {backbone_score} not\
+                    currently implemented.'
+            )
+        molecule_info = parse_residues(molecule, coordinators)
+        chunks = distribute(scores, kwargs['threads'])
+        args = [(molecule, chunks[idx], stats, gaussian_stats, molecule_info,
+                coordinators, residue_score, backbone_score, max_coordinators,
+                threshold, cnn_weight) for idx in range(kwargs['threads'])]
+        pool = multiprocessing.Pool(kwargs['threads'])
+        zeros = pool.starmap(analyse_probes, args)
+        new_zeros = []
+        for element in zeros:
+            for subelement in element:
+                new_zeros.append(subelement)
+
+        new_zeros = np.array(new_zeros)
+
+        return new_zeros, molecule
 
     def voxelize(
         self,
@@ -62,7 +310,40 @@ class Brigit():
         validitychecks: bool = False,
         channels: list = CHANNELS,
         **kwargs
-    ):
+    ) -> tuple:
+        """
+        Creates a voxelized representation of target that contains structural
+        information regarding the spatial distribution of its physico-chemical
+        properties. It is powered by the Moleculekit library.
+
+        Args:
+            target (str): Path to the PDB file or PDB code of the target
+                protein.
+            voxelsize (float, optional): Distance between the different points
+                in the voxelization. Defaults to 1.0.
+            buffer (int, optional): Space generated around the protein so that
+                the model might analyse its borders. Its value should be as big
+                as the outmost layer of the CNN model. Defaults to 6.
+            validitychecks (bool, optional): Whether the voxelization program
+                will check the integrity of the PDB file. Its best to set it to
+                False, specially in the case of screening. Defaults to False.
+            channels (list, optional): List of channels allowed for
+                voxelization. In any standard use the channels are limited by
+                the training of the CNN model and should not be changed.
+                However, in case anyone has developed their own model they can
+                be changed. Defaults to CHANNELS.
+
+        Raises:
+            TypeError: In case the target is neither a PDB code nor a Path to a
+                local PDB file.
+
+        Returns:
+            tuple: (final_vox, p_centers, nvoxels) First element is the
+                voxelized representation of the protein; second element, the
+                real cartesian coordinates of each of the voxels in the first
+                element; the third element, is the number of such voxels in
+                the image.
+        """
         if isinstance(target, str):
             protein = Molecule(target, validateElements=False)
         elif isinstance(target, Molecule):
@@ -152,110 +433,6 @@ class Brigit():
 
         return scores
 
-    def predict(
-        self,
-        target: str,
-        metal: str,
-        max_coordinators: int = 4,
-        outputfile: str = None,
-        cnn_threshold: float = 0.5,
-        combined_threshold: float = 0.5,
-        verbose: int = 1,
-        clustering_radius: float = 5.0,
-        cnn_weight: float = 0.5,
-        **kwargs
-    ) -> np.array:
-        start = time.time()
-        if not isinstance(target, str):
-            message = 'Target has to be:\n  a) string with PDB ID or\n'
-            message += '  b) path to local PDB file.'
-            raise TypeError(message)
-
-        if combined_threshold > cnn_threshold:
-            cnn_threshold = combined_threshold
-
-        if verbose == 1:
-            print(f'Voxelizing target: {target}', end='\n\n')
-
-        vox, p_centers, nvoxels = self.voxelize(target, **kwargs)
-
-        if verbose == 1:
-            print(f'\nCNN evaluation of target: {target}', end='\n\n')
-
-        scores = self.evaluate(vox, p_centers, **kwargs)
-
-        if verbose == 1:
-            print(f'\nCoordination analysis of target: {target}', end='\n\n')
-
-        scores, molecule = self.coordination_analysis(
-            target, max_coordinators, metal, scores, cnn_threshold,
-            verbose, kwargs['residue_score'], kwargs['backbone_score'],
-            cnn_weight, kwargs
-        )
-        best_scores = np.argwhere(scores[:, 3] > combined_threshold)
-        new_scores = np.zeros((len(best_scores), 4))
-        for idx, idx_score in enumerate(best_scores):
-            new_scores[idx, :] = scores[idx_score, :]
-        centers = self.clusterize(
-            new_scores, molecule, clustering_radius
-        )
-
-        if outputfile is None:
-            if target.endswith('.pdb'):
-                outputfile = target.split('.')[0]
-            else:
-                outputfile = target
-        self.create_PDB(
-            target, outputfile, new_scores, combined_threshold, centers,
-            molecule
-        )
-        self.check_clusters(centers, molecule, outputfile, kwargs['args'])
-        end = time.time()
-        print(f'Computation took {round(end-start, 2)} s.', end='\n\n')
-        return scores
-
-    def coordination_analysis(
-        self, target, max_coordinators, metal, scores, threshold,
-        verbose, residue_score, backbone_score, cnn_weight, kwargs
-    ):
-        zeros = np.zeros(np.shape(scores[:, 3]))
-        molecule = protein(target, True)
-        coordinators, stats, gaussian_stats = find_most_likely_coordinators(
-            metal, kwargs['residues']
-        )
-        if residue_score == 'discrete':
-            residue_score = discrete_score
-        elif residue_score == 'gaussian':
-            residue_score = gaussian_score
-        else:
-            raise IndexError(
-                f'Residue coordination scoring function: {residue_score} not\
-                    currently implemented.'
-            )
-
-        if backbone_score == 'discrete':
-            backbone_score = discrete_score
-        else:
-            raise IndexError(
-                f'Backbone coordination scoring function: {backbone_score} not\
-                    currently implemented.'
-            )
-        molecule_info = parse_residues(molecule, coordinators)
-        chunks = distribute(scores, kwargs['threads'])
-        args = [(molecule, chunks[idx], stats, gaussian_stats, molecule_info,
-                coordinators, residue_score, backbone_score, max_coordinators,
-                threshold, cnn_weight) for idx in range(kwargs['threads'])]
-        pool = multiprocessing.Pool(kwargs['threads'])
-        zeros = pool.starmap(analyse_probes, args)
-        new_zeros = []
-        for element in zeros:
-            for subelement in element:
-                new_zeros.append(subelement)
-
-        new_zeros = np.array(new_zeros)
-
-        return new_zeros, molecule
-
     def clusterize(
         self, scores, molecule, clustering_radius
     ):
@@ -281,7 +458,7 @@ class Brigit():
             cluster_mean = np.average(
                 cluster[:, :3], axis=0, weights=cluster[:, 3] ** 2
             )
-            score_mean = np.average(cluster[:, 3])
+            score_mean = np.sum(cluster[:, 3])
             result.add(cluster_mean, score_mean)
         return result
 
@@ -299,7 +476,7 @@ class Brigit():
                         if (
                             np.linalg.norm(
                                 atom - center
-                            ) < 3.5
+                            ) < 5
                         ):
                             if not coordinator_found:
                                 writer.write(f'{name},')
