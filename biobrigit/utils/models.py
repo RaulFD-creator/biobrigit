3261a2c07880f54dde9a9f90e4b33503527eef5e
Raúl Fernández Díaz <78709417+RaulFD-creator@users.noreply.github.com>
2022-12-14 11:34:28 +0100

    Eliminate DeepSite and TinyBrigit as specific arch

--- biobrigit/utils/models.py
+++ biobrigit/utils/models.py
@@ -101,49 +101,6 @@ class BaseModel(pl.LightningModule):
         self.log("log_loss", log_loss)
 
 
-class tinyBrigit(BaseModel):
-    def __init__(
-        self,
-        learning_rate: float,
-        neurons_layer: int = 32,
-        size: int = 12,
-        num_dimns: int = 6
-    ):
-        super().__init__()
-        self.learning_rate = learning_rate
-        self.example_input_array = torch.rand(
-            num_dimns*size**3, 1).view(1, num_dimns, size, size, size)
-
-        self.convolutional = nn.Sequential(
-            nn.Dropout3d(0.2),
-            conv3D(num_dimns, neurons_layer, 3, 2, nn.LeakyReLU, 0.2),
-            nn.BatchNorm3d(neurons_layer),
-            conv3D(neurons_layer, neurons_layer, 3, 1, nn.LeakyReLU, 0.2),
-            nn.BatchNorm3d(neurons_layer),
-            nn.AvgPool3d(2),
-
-            conv3D(neurons_layer, neurons_layer, 3, 1, nn.LeakyReLU, 0.2),
-            nn.BatchNorm3d(neurons_layer),
-            conv3D(neurons_layer, neurons_layer, 3, 1, nn.LeakyReLU, 0.2),
-            conv3D(neurons_layer, neurons_layer, 3, 1, nn.LeakyReLU, 0.2),
-            nn.BatchNorm3d(neurons_layer),
-            nn.AvgPool3d(2),
-            nn.Flatten()
-        )
-        self.classifier1 = nn.Sequential(
-            linear(864, 512, 0.2, nn.LeakyReLU),
-            linear(512, 256, 0.2, nn.LeakyReLU),
-        )
-        self.classifier2 = nn.Sequential(
-            linear(256, 1, 0.0, nn.Sigmoid)
-        )
-
-    def forward(self, x):
-        x = self.convolutional(x)
-        x = self.classifier1(x)
-        return self.classifier2(x)
-
-
 class BrigitCNN(BaseModel):
     def __init__(
         self,
@@ -186,44 +143,6 @@ class BrigitCNN(BaseModel):
         self.classifier2 = nn.Sequential(
             linear(256, 1, 0.0, nn.Sigmoid)
         )
-
-    def forward(self, x):
-        x = self.convolutional(x)
-        x = self.classifier1(x)
-        return self.classifier2(x)
-
-
-class DeepSite(BaseModel):
-    def __init__(
-        self,
-        learning_rate: float,
-        include: list = None,
-        exclude: list = None,
-        **kwargs
-    ):
-        super().__init__()
-        self.save_hyperparameters('learning_rate', 'include', 'exclude')
-
-        self.convolutional = nn.Sequential(
-            conv3D(6, 32, 8, 3, nn.ELU),
-            conv3D(32, 48, 4, 1, nn.ELU),
-            nn.MaxPool3d(2),
-            nn.Dropout(0.25),
-            conv3D(48, 64, 4, 1, nn.ELU),
-            conv3D(64, 96, 4, 1, nn.ELU),
-            nn.MaxPool3d(2),
-            nn.Dropout(0.25),
-            nn.Flatten()
-        )
-        self.classifier1 = nn.Sequential(
-            linear(96*1**3, 256, 0.5)
-        )
-        self.classifier2 = nn.Sequential(
-            linear(256, 1, 0.0, nn.Sigmoid)
-        )
-        self.learning_rate = learning_rate
-        self.example_input_array = torch.rand(
-            6*12*12*12, 1).view(1, 6, 12, 12, 12)
 
     def forward(self, x):
         x = self.convolutional(x)
